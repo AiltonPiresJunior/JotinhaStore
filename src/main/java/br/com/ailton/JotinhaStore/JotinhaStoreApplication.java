726abcce52d76915ca32e8a2c5f5b5dd387ea646
AiltonPiresJunior <ailtonsa.junior@outlook.com>
2022-01-24 15:47:52 -0300

    Criação do projeto

---
+++ src/main/java/br/com/ailton/JotinhaStore/JotinhaStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.ailton.JotinhaStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotinhaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotinhaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ailton/JotinhaStore/JotinhaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ailton.JotinhaStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotinhaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

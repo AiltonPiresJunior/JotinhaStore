b07b47e236b2de69057ff51bc58e7639457b9962
AiltonPiresJunior <89941573+AiltonPiresJunior@users.noreply.github.com>
2022-03-11 13:42:15 -0300

    Delete src directory

--- src/main/java/br/com/ailton/JotinhaStore/repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.ailton.JotinhaStore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.ailton.JotinhaStore.domain.Pedido;
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-	
-}
